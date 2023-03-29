62b3f9993c774b32e897e45de0d90f193f35cb50
BuildTools <colinjose4@gmail.com>
2023-03-29 22:23:38 +0200

    fix

--- Utils/ColorUtils.cs
+++ Utils/ColorUtils.cs
@@ -16,7 +16,7 @@ namespace Triangle3DAnimation.Utils
             }
 
             Random random = new Random();
-            float randomShading = random.NextSingle() * SHADING_DEFAULT_MAX;
+            float randomShading = random.NextSingle() * shadingIntensity;
             float r = color.X * (1f - randomShading);
             float g = color.Y * (1f - randomShading);
             float b = color.Z * (1f - randomShading);
